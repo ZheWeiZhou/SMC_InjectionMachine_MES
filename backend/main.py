414d05a3cb6abd61b9546f22793056eb77945736
ZheWeiZhou <g0975136415@gmail.com>
2025-05-12 13:28:10 +0800

    Fix API Cross issue

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
 import uvicorn
 from middleware.AuthCheck import AuthMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 from routers.historyrouter import historyrouter
 from routers.realtimedatarouter import realtimedatarouter
 from routers.commandrouter import commandrouter
 from routers.userrouter import userrouter
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.add_middleware(AuthMiddleware)
 
 app.include_router(historyrouter)
